eb4a3a4c1d76eabea7767fc89e3ba79828ac0e62
cailesi <cailesi@hidogame.com>
2019-05-10 21:49:15 +0800

    启动服务端

--- server/server.ts
+++ server/server.ts
@@ -5,20 +5,21 @@ let url = require("url");
 
 export default class Server {
     private static _route: Router = new Router();
+    private static _head: Object = {"Content-Type": "text/plain"};
 
-    static start(){
-        let onRequest = (request: any, response: any) => {
-            let pathName = url.parse(request.url).pathname;
-            console.log("Request for" + pathName + "recv！");
-    
-            this._route.route(pathName);
-    
-            response.writeHead(200, {"Content-Type": "text/plain"});
-            response.write("Hello");
-            response.end();
-        }
-    
-        http.createServer(onRequest).listen(8888);
-        console.log("Server has started.");
+    static createServer(){
+        return new Promise((resolve, reject) => {
+            let _self = this;
+            let onRequest = (req: any, res: any) => {
+                res.writeHead(200, _self._head);
+                let params = url.parse(req.url, true).query;
+                console.log(params.name)
+                res.write("网站名:" + params.name);
+                res.write("\n网站URL:" + params.url);
+                res.end();
+                resolve();
+            }
+            http.createServer(onRequest).listen(8888);
+        });
     }
 }
