fdcf26b15b6568d1dd563b27070308b192d59eec
cailesi <cailesi@hidogame.com>
2019-05-10 19:19:58 +0800

    ts node server test

---
+++ router.ts
@@ -0,0 +1,5 @@
+export default class Router {
+    route(pathname: string){
+        console.log("about to route a request for" + pathname);
+    }
+}
---
+++ server.ts
@@ -0,0 +1,24 @@
+import Router from "./router";
+
+let http = require("http");
+let url = require("url");
+
+export default class Server {
+    private static _route: Router = new Router();
+
+    static start(){
+        let onRequest = (request: any, response: any) => {
+            let pathName = url.parse(request.url).pathname;
+            console.log("Request for" + pathName + "recv！");
+    
+            this._route.route(pathName);
+    
+            response.writeHead(200, {"Content-Type": "text/plain"});
+            response.write("Hello");
+            response.end();
+        }
+    
+        http.createServer(onRequest).listen(8888);
+        console.log("Server has started.");
+    }
+}
