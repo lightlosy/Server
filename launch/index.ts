f7e8e1b0b5b9f45456625a92e447d34d8a9f8ca5
lightlosy <853517173@qq.com>
2019-05-11 13:45:05 +0800

    comi

--- launch/index.ts
+++ launch/index.ts
@@ -2,10 +2,6 @@ import Server from "../server/server";
 import File from "../file/file";
 import { Path } from "../path/path";
 
-// Server.start();
-// File.writeFile(Path.User, "aa").then(() => {
-//     File.readFileAsync(Path.User);
-// });
 Server.createServer();
 console.log("当前目录:", __dirname);
 console.log("当前版本:", process.version);
